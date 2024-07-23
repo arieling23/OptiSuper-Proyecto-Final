a3325517e4929d49b592bcb3609c956d6a9e104b
Kevin13201j <rivadeneirakevin2001@gmail.com>
2024-07-22 23:35:26 -0500

    Actualización Success

--- frontend/src/components/Success.js
+++ frontend/src/components/Success.js
@@ -12,10 +12,10 @@ const Success = () => {
   return (
     <div className="success-container">
       <div className="success-message">
-        <h1>Registration Successful!</h1>
-        <p>Your account has been created successfully.</p>
-        <p><a href="/login">Click here to login.</a></p>
-        <button onClick={handleBackToHome} className="back-to-home">Back to Home</button>
+        <h1>¡Registro Exitoso!</h1>
+        <p>Tu cuenta ha sido creada exitosamente.</p>
+        <p><a href="/login">Haz clic aquí para iniciar sesión.</a></p>
+        <button onClick={handleBackToHome} className="back-to-home">Volver a la Página Principal</button>
       </div>
     </div>
   );
